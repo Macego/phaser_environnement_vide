6767b20551c796a5082a5af8a03632ae79396ebc
Macéo Mohamed <mohamedleraymaceo@gmail.com>
2026-03-15 16:17:58 +0100

    mort dans le vide, this undefined dans niveauTermine, meilleur score affiché + relance au clavier

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ var nombreDeSauts = 0;
 var meilleurScore = 0;
 var scoreText;
 var zoneFin;
-var debugCoordsText;
+
 
 
 /***********************************************************************/
@@ -90,21 +90,13 @@ function create() {
   player.setBounce(0);
   player.setCollideWorldBounds(true);
 
-  zoneFin = this.add.zone(1700, 200, 60, 80);
+  zoneFin = this.add.zone(3155, 136, 60, 80);
 this.physics.world.enable(zoneFin);
 zoneFin.body.setAllowGravity(false);
 zoneFin.body.setImmovable(true);
 
 this.physics.add.overlap(player, zoneFin, niveauTermine, null, this);
 
-
-var debugZone = this.add.rectangle(1700, 200, 60, 80, 0x00ff00, 0.4);
-
-debugCoordsText = this.add.text(16, 100, "", {
-  fontSize: "18px",
-  fill: "#00FF00"
-});
-debugCoordsText.setScrollFactor(0);
 
 
 
@@ -231,7 +223,7 @@ if (Phaser.Input.Keyboard.JustDown(bouton_reset)) {
 
 }
 
-debugCoordsText.setText("x: " + Math.floor(player.x) + "  y: " + Math.floor(player.y));
+
 
 
 // pause / reprise (bouton S)
